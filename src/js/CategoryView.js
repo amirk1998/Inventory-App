4082986aaf6ba0ce00aa109cd3355dc63edd661b
amirk1998 <amirk1998.pv@gmail.com>
2023-01-23 19:16:32 +0330

    CategoryView Class has been updated : Part 01

--- src/js/CategoryView.js
+++ src/js/CategoryView.js
@@ -1,1 +1,26 @@
+import Storage from './Storage.js';
 //  title , description => saveCategory => ...
+const title = document.querySelector('#category-title');
+const description = document.querySelector('#category-description');
+const addNewCategoryBtn = document.querySelector('#add-new-category');
+
+export default class CategoryView {
+  //
+  constructor() {
+    //
+    addNewCategoryBtn.addEventListener('click', (event) => this.addNewCategory(event));
+    this.categories = [];
+  }
+
+  addNewCategory(event) {
+    //
+    event.preventDefault();
+    const title = title.value;
+    const description = description.value;
+    if (!title || !description) return;
+    Storage.saveCategory({ title, description });
+    this.categories = Storage.getAllCategories();
+    // update DOM : update select option in category
+    // 161
+  }
+}
